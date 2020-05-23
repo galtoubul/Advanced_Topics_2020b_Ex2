de62b72cfb6c21e32e904f5e920d1a2828467430
galtoubul <galtoubul@gmail.com>
2020-05-23 11:22:24 +0300

    using command line args

--- Simulator/main.cpp
+++ Simulator/main.cpp
@@ -1,12 +1,23 @@
+#ifdef WINDOWS
+#include <direct.h>
+#define GetCurrentDir _getcwd
+#else
+#include <unistd.h>
+#define GetCurrentDir getcwd
+#endif
+
 #include <cstdlib>
 #include <tuple>
 #include <iostream>
+#include <fstream>
 #include <dlfcn.h>
 #include <map>
 #include <memory>
+#include <filesystem>
 #include <string>
 #include "Simulation.h"
 #include "Registrar.h"
+#include "../Common/Parser.h"
 using std::cout;
 using std::cin;
 using std::endl;
@@ -14,15 +25,8 @@ using std::unique_ptr;
 using std::pair;
 #define NOT_LEGAL -1
 #define LEGAL 1
-
-int check(int num, const std::string& mode){
-    if (num < 0){
-        TRAVELS_OR_ALGORITHMS_NUMBER_ERROR(mode)
-        cout << "Please reenter the number you would like to test" << endl;
-        return NOT_LEGAL;
-    }
-    return LEGAL;
-}
+namespace fs = std::filesystem;
+#define SEPERATOR string(1, fs::path::preferred_separator)
 
 // this is a deleter that will be operated with the "dying" of the unique_ptr
 // and by that dlclose will occur
@@ -34,9 +38,50 @@ struct DLCloser{
     }
 };
 
+string getCurrentDir() {
+    char buff[FILENAME_MAX]; //create string buffer to hold path
+    GetCurrentDir(buff, FILENAME_MAX);
+    string currentWorkingDir(buff);
+    return currentWorkingDir;
+}
 
-int main() {
-    cout << "Welcome to Stowage Algorithm Simulator" << endl;
+void getPaths (int argc, char** argv, string& travelPath, string& algorithmPath, string& output){
+    std::vector<std::string> args(argv, argv + argc);
+    for (size_t i = 1; i < args.size(); ++i) {
+        if (args[i] == "-algorithm_path")
+            algorithmPath = args[i + 1];
+        if (args[i] == "-output")
+            output = args[i + 1];
+        if (args[i] == "-travel_path")
+            travelPath = args[i + 1];
+    }
+
+    if (output.empty())
+        output = getCurrentDir();
+
+    if (algorithmPath.empty()){
+        fs::create_directory(output + SEPERATOR + "Algorithms");
+        algorithmPath = output + SEPERATOR + "Algorithms";
+        for (const auto & entry : fs::directory_iterator(output)){
+            string fileName = entry.path().string();
+            if(string(".so") == (fileName.string::substr(fileName.size() - 3))){
+                string algorithmName = fileName.string::substr(fileName.find_last_of(SEPERATOR) + 1);
+                algorithmPath += SEPERATOR += algorithmName;
+                fs::copy(fileName, algorithmPath);
+            } // TODO: handle an error of not getting so files
+        }
+    }
+}
+
+
+int main(int argc, char** argv) {
+
+    string travelPath, algorithmPath, output;
+    getPaths(argc, argv, travelPath, algorithmPath, output);
+    if(travelPath.empty()){
+        cout << "Fatal Error: missing -travel_path argument. Exiting..." << endl;
+        return EXIT_FAILURE;
+    }
 
     unique_ptr<void, DLCloser> handle(dlopen("../Algorithm/_308394642_a.so",RTLD_LAZY)); // TODO: enter path from command line args
     if(!handle){
